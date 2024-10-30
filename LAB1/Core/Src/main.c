77bf63df937c3c2b095270f71d8fb30fb886d53f
DESKTOP-0ENLD38\ADMIN <tu.bui1911@hcmut.edu.vn>
2024-10-30 18:11:13 +0700

    update for EX6

--- LAB1/Core/Src/main.c
+++ LAB1/Core/Src/main.c
@@ -95,94 +95,54 @@ int main(void)
     while (1)
     {
       /* USER CODE END WHILE */
-  	  if(counter == 1)
-  	  	  {
-  		  	  HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, SET);
-  	  		  HAL_GPIO_WritePin(LED12_GPIO_Port, LED12_Pin, RESET);
-  		  	  counter++;
-  		  	  HAL_Delay(1000);
-  	  	  }
-  	  if(counter == 2)
-  	  	  {
-  	  		  HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, SET);
-  	  		  HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, RESET);
-  	  		  counter++;
-  	  		  HAL_Delay(1000);
-  	  	  }
-  	  if(counter == 3)
-  	  	  {
-  	  		  HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, SET);
-  	  		  HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, RESET);
-  	  		  counter++;
-  	  		  HAL_Delay(1000);
-  	  	  }
-  	  if(counter == 4)
-  	  	  {
-  	  		  HAL_GPIO_WritePin(LED4_GPIO_Port, LED4_Pin, SET);
-  	  		  HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, RESET);
-  	  		  counter++;
-  	  		  HAL_Delay(1000);
-  	  	  }
-  	  if(counter == 5)
-  	  	  {
-  	  		  HAL_GPIO_WritePin(LED5_GPIO_Port, LED5_Pin, SET);
-  	  		  HAL_GPIO_WritePin(LED4_GPIO_Port, LED4_Pin, RESET);
-  	  		  counter++;
-  	  		  HAL_Delay(1000);
-  	  	  }
-  	  if(counter == 6)
-  	  	  {
-  	  		  HAL_GPIO_WritePin(LED6_GPIO_Port, LED6_Pin, SET);
-  	  		  HAL_GPIO_WritePin(LED5_GPIO_Port, LED5_Pin, RESET);
-  	  		  counter++;
-  	  		  HAL_Delay(1000);
-  	  	  }
-  	  if(counter == 7)
-  	  	  {
-  	  		  HAL_GPIO_WritePin(LED7_GPIO_Port, LED7_Pin, SET);
-  	  		  HAL_GPIO_WritePin(LED6_GPIO_Port, LED6_Pin, RESET);
-  	  		  counter++;
-  	  		  HAL_Delay(1000);
-  	  	  }
-  	  if(counter == 8)
-  	  	  {
-  	  		  HAL_GPIO_WritePin(LED8_GPIO_Port, LED8_Pin, SET);
-  	  		  HAL_GPIO_WritePin(LED7_GPIO_Port, LED7_Pin, RESET);
-  	  		  counter++;
-  	  		  HAL_Delay(1000);
-  	  	  }
-  	  if(counter == 9)
-  	  	  {
-  	  		  HAL_GPIO_WritePin(LED9_GPIO_Port, LED9_Pin, SET);
-  	  		  HAL_GPIO_WritePin(LED8_GPIO_Port, LED8_Pin, RESET);
-  	  		  counter++;
-  	  		  HAL_Delay(1000);
-  	  	  }
-  	  if(counter == 10)
-  	  	  {
-  	  		  HAL_GPIO_WritePin(LED10_GPIO_Port, LED10_Pin, SET);
-  	  		  HAL_GPIO_WritePin(LED9_GPIO_Port, LED9_Pin, RESET);
-  	  		  counter++;
-  	  		  HAL_Delay(1000);
-  	  	  }
-  	  if(counter == 11)
-  	  	  {
-  	  		  HAL_GPIO_WritePin(LED11_GPIO_Port, LED11_Pin, SET);
-  	  		  HAL_GPIO_WritePin(LED10_GPIO_Port, LED10_Pin, RESET);
-  	  		  counter++;
-  	  		  HAL_Delay(1000);
-  	  	  }
-  	  if(counter == 12)
-  	  	  {
-  	  		  HAL_GPIO_WritePin(LED12_GPIO_Port, LED12_Pin, SET);
-  	  		  HAL_GPIO_WritePin(LED11_GPIO_Port, LED11_Pin, RESET);
-  	  		  counter = 1;
-  	  		  HAL_Delay(1000);
-  	  	  }
+  	  switch(counter++)
+  	  {
+  	  	case 1:
+  		  	  HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, 1);
+  		  	  break;
+  	  	case 2:
+  	  		  HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, 1);
+  			  break;
+  		case 3:
+  	  		  HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, 1);
+  			  break;
+  		case 4:
+  	  		  HAL_GPIO_WritePin(LED4_GPIO_Port, LED4_Pin, 1);
+  			  break;
+  		case 5:
+  	  		  HAL_GPIO_WritePin(LED5_GPIO_Port, LED5_Pin, 1);
+  			  break;
+  		case 6:
+  	  		  HAL_GPIO_WritePin(LED6_GPIO_Port, LED6_Pin, 1);
+  			  break;
+  		case 7:
+  	  		  HAL_GPIO_WritePin(LED7_GPIO_Port, LED7_Pin, 1);
+  			  break;
+  		case 8:
+  	  		  HAL_GPIO_WritePin(LED8_GPIO_Port, LED8_Pin, 1);
+  			  break;
+  		case 9:
+  	  		  HAL_GPIO_WritePin(LED9_GPIO_Port, LED9_Pin, 1);
+  			  break;
+  		case 10:
+  	  		  HAL_GPIO_WritePin(LED10_GPIO_Port, LED10_Pin, 1);
+  			  break;
+  		case 11:
+  	  		  HAL_GPIO_WritePin(LED11_GPIO_Port, LED11_Pin, 1);
+  			  break;
+  		case 12:
+  	  		  HAL_GPIO_WritePin(LED12_GPIO_Port, LED12_Pin, 1);
+  			  break;
+  	  }
+  		if(counter == 13)
+  		{
+  			counter = 1;
+  		}
+  		HAL_Delay(1000);
+  	  }
       /* USER CODE BEGIN 3 */
-    }
     /* USER CODE END 3 */
-  }
+}
 
 /**
   * @brief System Clock Configuration
