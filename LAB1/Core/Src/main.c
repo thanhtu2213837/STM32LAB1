f75b806ba50cdb69a434a12597c3d793eb1a0474
DESKTOP-0ENLD38\ADMIN <tu.bui1911@hcmut.edu.vn>
2024-10-31 00:45:47 +0700

    EX9

--- LAB1/Core/Src/main.c
+++ LAB1/Core/Src/main.c
@@ -106,6 +106,48 @@ int main(void)
 	  		  break;
 	  }
   }
+  void clearNumberOnClock(int num)
+  {
+	  switch(num)
+	  {
+	  case 1:
+	  		  HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, 0);
+	  		  break;
+	  case 2:
+	  		  HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, 0);
+	  		  break;
+	  case 3:
+	  		  HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, 0);
+	  		  break;
+	  case 4:
+	  		  HAL_GPIO_WritePin(LED4_GPIO_Port, LED4_Pin, 0);
+	  		  break;
+	  case 5:
+	  		  HAL_GPIO_WritePin(LED5_GPIO_Port, LED5_Pin, 0);
+	  		  break;
+	  case 6:
+	  		  HAL_GPIO_WritePin(LED6_GPIO_Port, LED6_Pin, 0);
+	  		  break;
+	  case 7:
+	  		  HAL_GPIO_WritePin(LED7_GPIO_Port, LED7_Pin, 0);
+	  		  break;
+	  case 8:
+	  		  HAL_GPIO_WritePin(LED8_GPIO_Port, LED8_Pin, 0);
+	  		  break;
+	  case 9:
+	  		  HAL_GPIO_WritePin(LED9_GPIO_Port, LED9_Pin, 0);
+	  		  break;
+	  case 10:
+	  		  HAL_GPIO_WritePin(LED10_GPIO_Port, LED10_Pin, 0);
+	  		  break;
+	  case 11:
+	  		  HAL_GPIO_WritePin(LED11_GPIO_Port, LED11_Pin, 0);
+	  		  break;
+	  case 12:
+	  		  HAL_GPIO_WritePin(LED12_GPIO_Port, LED12_Pin, 0);
+	  		  break;
+	  }
+  }
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -136,12 +178,13 @@ int main(void)
     while (1)
     {
       /* USER CODE END WHILE */
-      setNumberOnClock(num++);
+      setNumberOnClock(num);
+      HAL_Delay(1000);
+      clearNumberOnClock(num++);
 	  if(num == 13)
 	  {
 		  num = 1;
 	  }
-      HAL_Delay(1000);
       /* USER CODE BEGIN 3 */
     }
     /* USER CODE END 3 */
